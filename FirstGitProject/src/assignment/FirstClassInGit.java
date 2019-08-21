a783a992606fb2959b493fefc16e2fa24185f233
mp07935 <mp07935@CEIT2553214X021.ad.georgiasouthern.edu>
2019-08-21 16:45:48 -0400

    Added console input and display of last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,9 +1,14 @@
 package assignment;
+import java.util.Scanner;
 
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello my name is Mason");
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What is your last name?");
+		String lastName = keyboard.nextLine();
+		System.out.println("Your last name is " + lastName);
 		
 		//Then commit your changes
 
